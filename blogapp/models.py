ac3f099834d8b7f1d9cd2b5b30614882ef5c4461
Enoch Nuertey <enochnuer111@gmail.com>
2025-01-14 23:56:12 +0000

    registered models to admin

--- blogapp/models.py
+++ blogapp/models.py
@@ -41,6 +41,8 @@ class BlogPost(models.model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title} by {self.author}" 
